7a38ea11dd0c2c344a0779c04f562339075503fe
henriquedouradoo <henrique.domingos@sptech.school>
2025-12-15 20:39:48 -0300

    feat: Coloca Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/model/NinjaModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas.model;
 
 import dev.java10x.CadastroDeNinjas.Missoes.model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
